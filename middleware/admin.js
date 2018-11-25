7bf7614868c28fec5753ed5cb1e3baeed6b74e2e
arulk8 <arulmurugan194@gmail.com>
2018-11-25 20:02:58 +0530

    added some comments

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,13 +1,18 @@
-
 let admin = (req, res, next) => {
-    if (!req.user.isAdmin){ 
-        res.status(403).send("Access Denied. you don't have sufficient previladge to perform this action" );
-        return false;
-    }
-    next();
-
-}
+  if (!req.user.isAdmin) {
+    res
+      .status(403)
+      .send(
+        "Access Denied. you don't have sufficient previladge to perform this action"
+      );
+    return false;
+  }
+  next();
+};
 
 module.exports = admin;
 // 400 - bad request, 401- unauthorized, 403- forbidden,
-// 409-conflict,500-internal error,200 - ok 
+// 409-conflict,500-internal error,200 - ok
+// 400 to 499 expected error. other are un expected error
+// expected error - invalid id, already deleted, invalid data
+// unexpected - bugs in backend, DB error,network problem, invalid url.
