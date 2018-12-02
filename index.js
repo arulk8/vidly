29932ada1502deeb5dad87d9ae96e7f7e3ef365d
arulk8 <arulmurugan194@gmail.com>
2018-12-02 09:41:00 +0530

    added CORS and visibility to x-auth-token

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const express = require('express');
-const config = require('config');
-const helmet = require('helmet');
-const morgan = require('morgan'); // used for logging  errors
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const express = require("express");
+const config = require("config");
+const helmet = require("helmet");
+const morgan = require("morgan"); // used for logging  errors
 // var Fawn = require("fawn"); for providing transaction in mongoDB.
 // const startupDebugger = require('debug')('app:startup');
-// const dbDebugger = require('debug')('app:db'); 
+// const dbDebugger = require('debug')('app:db');
 //const authentication = require('./middleware/authenticate');
 //const logger = require('./middleware/logger');
-const logger = require('./startup/logging');
-require('./startup/config')(logger);
+const logger = require("./startup/logging");
+require("./startup/config")(logger);
 //console.log(config.get('jwtPrivateKey'));
-require('./startup/DBconnection')(logger);
+require("./startup/DBconnection")(logger);
 
 // throw new Error("i created this error");  // checking error handelling of unhandeled exception.
 
- /*const p = Promise.reject(
+/*const p = Promise.reject(
      setTimeout( ()=>{
         throw new Error("i created this error");
      }, 100)
@@ -28,19 +28,20 @@ require('./startup/DBconnection')(logger);
 // Fawn.init(mongoose);
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views','./views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 app.use(express.json());
 //app.use(authentication);
 //app.use(logger);
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(helmet());
-app.use(morgan('tiny'));
-require('./startup/routes')(app)
+app.use(morgan("tiny"));
+require("./startup/routes")(app);
+require("./startup/cors")(app);
 //startupDebugger('morgan is enabled');
 //dbDebugger('db is enabled');
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`listening on ${ port }...`)
+  console.log(`listening on ${port}...`);
 });
